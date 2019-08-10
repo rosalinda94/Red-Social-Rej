bb5171ebfb87f42a0745f5943c27e73f540872a4
Emilia Segura <emiliasegura@Emilias-MacBook-Pro.local>
2019-08-09 21:19:12 -0300

    perfil2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,22 +119,8 @@
             @yield('content')
             @yield('nosotros')
         </main>
+            @yield('footer')
     </div>
-
-    <footer>
-  <nav  class="footer">
-  <div class="empresa">
-    <p clas="derechos">2019 Mi Club. Todos los derechos reservados.</p>
-  </div>
-  <div class="redes">
-    <ul>
-      <a href="http://www.twitter.com"><i class="fab fa-twitter  fa-lg" style=""></i></a>
-      <a href="http://www.facebook.com"><i class="fab fa-facebook-square fa-lg" style=""></i></a>
-      <a href="http://www.instagram.com"><i class="fab fa-instagram fa-lg" style=""></i></a>
-    </ul>
-  </div>
- </nav>
-</footer>
 
 </body>
 </html>
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,11 +1,35 @@
 @extends('layouts.app')
 
+@push('styles')
+<link href="{{ asset('css/logged/profileLogged.css') }}" rel="stylesheet">
+@endpush
+
 @section('content')
-
-<div class="fotoPortada">
-  <form class=""  method="post">
-
-  </form>
+  <div class="perfilLogueado">
+<section class="portadaUsuario">
+<div class="infoUsuario">
+  <div class="cuadrado4">
+    <img id="fotoPerfil4" src="storage\{{ Auth::user()->person->avatar }}"  alt="">
+  </div>
+  <h3 class="user-name">{{Auth::user()->person->name }} {{Auth::user()->person->lastName }}</h3>
 </div>
+  <form class="" action="index" method="post" enctype="multipart/form-data">
+    <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('Compartí tus imagenes') }}</label>
+    <input id="avatar" type="file" class=" @error('avatar') is-invalid @enderror" accept="image/*" name="avatar" value="{{ old('avatar') }}" required autocomplete="avatar" autofocus>
+  </form>
+</section>
 
+<section class="datosUsuario">
+<article class="infoGeneral">
+  <a href="">Ingresá tus datos!</a>
+</article>
+<article class="fotosUsuario">
+
+</article>
+
+</section>
+<section class="posteosUsuario">
+
+</section>
+  </div>
 @endsection
