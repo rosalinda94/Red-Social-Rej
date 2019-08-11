a835cf9aafe5dc3c78de9b8b03504c263b0ea2b0
Emilia Segura <emiliasegura@Emilias-MacBook-Pro.local>
2019-08-11 20:13:57 -0300

    perfil

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -13,9 +13,9 @@
   </div>
   <h3 class="user-name">{{Auth::user()->person->name }} {{Auth::user()->person->lastName }}</h3>
 </div>
-  <form class="" action="index" method="post" enctype="multipart/form-data">
-    <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('Compartí tus imagenes') }}</label>
-    <input id="avatar" type="file" class=" @error('avatar') is-invalid @enderror" accept="image/*" name="avatar" value="{{ old('avatar') }}" required autocomplete="avatar" autofocus>
+  <form class="fotoPortada" action="index" method="post" enctype="multipart/form-data">
+    <label for="avatar" class="labelAvatar">{{ __('Foto de portada') }}</label>
+    <input style='display: none;' id="avatar" type="file" class=" @error('avatar') is-invalid @enderror" accept="image/*" name="avatar" value="{{ old('avatar') }}" required autocomplete="avatar" autofocus>
   </form>
 </section>
 
