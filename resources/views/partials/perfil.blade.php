1aa6e599d1647186f1dd36bc67a64e8ab76f977c
Emilia Segura <emiliasegura@Emilias-MacBook-Pro.local>
2019-08-12 00:42:57 -0300

    posteos con css

--- resources/views/partials/perfil.blade.php
+++ resources/views/partials/perfil.blade.php
@@ -1,72 +1,75 @@
+@push('styles')
+<link href="{{ asset('css/logged/publicacion.css') }}" rel="stylesheet">
+@endpush
+
  <article class="publicacion">
       <ul>
         <!-- pasar solo estooo -->
-
       @forelse($posts as $post)
-      
-      @if (auth()->id()==$post->user_id)
       <article class="publicacion">
-          <div class="">
-
-
-              <a class="profile-picture" href="profile/profile"><img src="{{ Storage::url(Auth::user()->person->avatar) }}"  alt="" width="50px"></a>
-
-
-              <b>{{$post->user->name}}</b><br>
-              @isset($post->group)
-              <p>Este post pertenece a la categoria :<b> {{$post->group->name}}</b></p>
-
-              @endisset
-
-
-              <p>{{$post->body }}</p>
-
-              <img src="/storage/{{$post->image}}"  alt="" width="300px" height="300px">
-              <br> <br>
+        @if (auth()->id()==$post->user_id)
+          <form class="eliminar" action="{{URL::to('/')}}/post/{{ $post->id }}" method="POST">
+             {{ csrf_field() }}
+             {{ method_field('DELETE') }}
+             <button id="eliminar" type="submit"><i class="fas fa-trash-alt"></i></button>
+           </form>
+        @endif
+          <div class="quienPublica">
+              <a class="cuadrado5" href="profile/profile"><img id="fotoPerfil5" src="{{ Storage::url(Auth::user()->person->avatar) }}"  alt="" width="50px"></a>
+              <div class="quienPublicaInfo">
+                <h4 class="userName" >{{$post->user->name}}</h4>
+                @isset($post->group)
+                <p>Este post pertenece a la categoria :<b> {{$post->group->name}}</b></p>
+                @endisset
               </div>
+            </div>
+
+              <p class="textoPublicacion">{{$post->body }}</p>
+
+              <img class="imgPublicacion" src="/storage/{{$post->image}}"  alt="">
+              <br> <br>
+
                 <div id='publicacion-user' class="publicacion-user">
                   <div class="botones">
-                    <a id='like-button' class='like-button' href="#"><i class="far fa-thumbs-up"> Me gusta</i></a>
-                    <a id='dislike-button' class='dislike-button' href="#"><i class="far fa-thumbs-down"> No me gusta</i></a>
-                    <a id='share-button' class='share-button' href="#"><i class="fas fa-share">  Compartir</i></a>
+                    <a id='like-button' class='like-button' href="#"><i class="fas fa-thumbs-up"></i>Me gusta</a>
+                    <a id='dislike-button' class='dislike-button' href="#"><i class="fas fa-thumbs-down"></i>No me gusta</a>
+                    <a id='share-button' class='share-button' href="#"><i class="fas fa-share"></i>Compartir</a>
                   </div>
                   <div id='interaccion' class="interaccion">
                     <!-- Aca va la informacion sobre la interaccion del usuario con el posteo -->
-                    <i><?=
+                    <br>
+                    <i class="compartido"><?=
                     $numero_aleatorio = rand(1,5) . ' veces compartidos'; ?></i>
                   </div>
                 </div>
 
-              @if (auth()->id()==$post->user_id)
-                <form action="{{URL::to('/')}}/post/{{ $post->id }}" method="POST">
-                   {{ csrf_field() }}
-                   {{ method_field('DELETE') }}
-                   <button type="submit">Eliminar</button>
-                 </form>
-              @endif
+
 
                   @forelse($comments as $comment)
                   @if ($post->id == $comment->post_id)
-                 <b>{{$post->user->name }}</b> 
+                 <b>{{$post->user->name }}</b>
                 <p>{{$comment->body }}</p>
               @endif
-                   
+
 
                 @empty
                 @endforelse
               <div class="form-row">
               <div class="form-group col-md-12" style="justify-content: center">
                 <form action="comment/create" method="POST">
-                 
+
                    {{ csrf_field() }}
-                   <input type="hidden" name="postid" value="1">
-                    <input type="text" class="form-control" id="body" name="body" placeholder="Deja aca tu comentario">
+
+                   <input type="hidden" value='1' name="postId">
+                    <input type="text" class="" id="bodyComentarios" name="body" placeholder="Deja acá tu comentario">
+                   {{-- <input type="hidden" name="postid" value="1">
+                    <input type="text" class="form-control" id="body" name="body" placeholder="Deja aca tu comentario"> --}}
                  </form>
               </div>
 
               </div>
             </article>
- @endif
+
       @empty
       <li><br>No existen posteos </b><img src="/img/triste.jpg" width="150px"></li>
       @endforelse
@@ -74,5 +77,5 @@
 </ul>
 
       {{ $posts->appends(request()->query())->links() }}
-      
+
        </article>
--- resources/views/partials/publicaciones.blade.php
+++ resources/views/partials/publicaciones.blade.php
@@ -1,46 +1,49 @@
+
+@push('styles')
+<link href="{{ asset('css/logged/publicacion.css') }}" rel="stylesheet">
+@endpush
  <article class="publicacion">
       <ul>
       	<!-- pasar solo estooo -->
 			@forelse($posts as $post)
 			<article class="publicacion">
-			    <div class="">
+        @if (auth()->id()==$post->user_id)
+          <form class="eliminar" action="{{URL::to('/')}}/post/{{ $post->id }}" method="POST">
+             {{ csrf_field() }}
+             {{ method_field('DELETE') }}
+             <button id="eliminar" type="submit"><i class="fas fa-trash-alt"></i></button>
+           </form>
+        @endif
+			    <div class="quienPublica">
+              <a class="cuadrado5" href="profile/profile"><img id="fotoPerfil5" src="{{ Storage::url(Auth::user()->person->avatar) }}"  alt="" width="50px"></a>
+              <div class="quienPublicaInfo">
+                <h4 class="userName" >{{$post->user->name}}</h4>
+                @isset($post->group)
+                <p>Este post pertenece a la categoria :<b> {{$post->group->name}}</b></p>
+                @endisset
+              </div>
+            </div>
 
+              <p class="textoPublicacion">{{$post->body }}</p>
 
-              <a class="profile-picture" href="profile/profile"><img src="{{ Storage::url(Auth::user()->person->avatar) }}"  alt="" width="50px"></a>
-
-
-			        <b>{{$post->user->name}}</b><br>
-              @isset($post->group)
-              <p>Este post pertenece a la categoria :<b> {{$post->group->name}}</b></p>
-
-              @endisset
-
-
-              <p>{{$post->body }}</p>
-
-              <img src="/storage/{{$post->image}}"  alt="" width="300px" height="300px">
+              <img class="imgPublicacion" src="/storage/{{$post->image}}"  alt="">
               <br> <br>
-			        </div>
+
                 <div id='publicacion-user' class="publicacion-user">
                   <div class="botones">
-                    <a id='like-button' class='like-button' href="#"><i class="far fa-thumbs-up"> Me gusta</i></a>
-                    <a id='dislike-button' class='dislike-button' href="#"><i class="far fa-thumbs-down"> No me gusta</i></a>
-                    <a id='share-button' class='share-button' href="#"><i class="fas fa-share">  Compartir</i></a>
+                    <a id='like-button' class='like-button' href="#"><i class="fas fa-thumbs-up"></i>Me gusta</a>
+                    <a id='dislike-button' class='dislike-button' href="#"><i class="fas fa-thumbs-down"></i>No me gusta</a>
+                    <a id='share-button' class='share-button' href="#"><i class="fas fa-share"></i>Compartir</a>
                   </div>
                   <div id='interaccion' class="interaccion">
                     <!-- Aca va la informacion sobre la interaccion del usuario con el posteo -->
-                    <i><?=
+                    <br>
+                    <i class="compartido"><?=
                     $numero_aleatorio = rand(1,5) . ' veces compartidos'; ?></i>
                   </div>
                 </div>
 
-              @if (auth()->id()==$post->user_id)
-                <form action="{{URL::to('/')}}/post/{{ $post->id }}" method="POST">
-                   {{ csrf_field() }}
-                   {{ method_field('DELETE') }}
-                   <button type="submit">Eliminar</button>
-                 </form>
-              @endif
+
 
                   @forelse($comments as $comment)
                   @if ($post->id == $comment->post_id)
@@ -56,13 +59,11 @@
                 <form action="comment/create" method="POST">
 
                    {{ csrf_field() }}
-<<<<<<< HEAD
+
                    <input type="hidden" value='1' name="postId">
-                    <input type="text" class="form-control" id="body" name="body" placeholder="Deja acá tu comentario">
-=======
-                   <input type="hidden" name="postid" value="1">
-                    <input type="text" class="form-control" id="body" name="body" placeholder="Deja aca tu comentario">
->>>>>>> b810ac9d455904347bfbe992a491e2ea1b9b41e0
+                    <input type="text" class="" id="bodyComentarios" name="body" placeholder="Deja acá tu comentario">
+                   {{-- <input type="hidden" name="postid" value="1">
+                    <input type="text" class="form-control" id="body" name="body" placeholder="Deja aca tu comentario"> --}}
                  </form>
               </div>
 
