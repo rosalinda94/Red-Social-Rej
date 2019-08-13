c0ab64ef318f5af70a069a57b2481a278e373735
Emilia Segura <emiliasegura@Emilias-MacBook-Pro.local>
2019-08-13 15:44:26 -0300

    resp tablet

--- resources/views/partials/perfil.blade.php
+++ resources/views/partials/perfil.blade.php
@@ -31,11 +31,11 @@
               <br> <br>
 
                 <div id='publicacion-user' class="publicacion-user">
-                  <div class="botones">   
-                  <a id='like-button' class='like-button' href="#"><i class="fas fa-thumbs-up"></i>Me gusta</a>
-                  <a id='dislike-button' class='dislike-button' href="#"><i class="fas fa-thumbs-down"></i>No me gusta</a>
-                  
-                    <a id='share-button' class='share-button' href="#"><i class="fas fa-share"></i>Compartir</a>
+                  <div class="botones">
+                  <a id='like-button' class='like-button' href="#"><i class="fas fa-thumbs-up"></i><span class="likes">Me gusta</span></a>
+                  <a id='dislike-button' class='dislike-button' href="#"><i class="fas fa-thumbs-down"></i></i><span class="likes">No me gusta</span></a>
+
+                    <a id='share-button' class='share-button' href="#"><i class="fas fa-share"></i></i><span class="likes">compartir</span></a>
                   </div>
                   <div id='interaccion' class="interaccion">
                     <!-- Aca va la informacion sobre la interaccion del usuario con el posteo -->
